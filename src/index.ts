b55bacb2029016ebda5e2dbc0595d3efe80eb7f1
sumankavibala <sumankavibala@gmail.com>
2026-04-12 13:24:03 +0530

    safe pool disconnection code update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { initializeDb } from './config/db';
+import { initializeDb, pool } from './config/db';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +9,11 @@ app.use(cors());
 app.use(express());
 
 initializeDb();
+
+process.on('SIGINT', async()=>{
+  await pool.end();
+  process.exit();
+})
 
 app.listen(PORT, ()=>{
   console.log(`Server is running on port ${PORT}`);
